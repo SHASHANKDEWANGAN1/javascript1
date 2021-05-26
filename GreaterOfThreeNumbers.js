db651cf58f0efa81f305234a09a74436ca1029eb
SHASHANKDEWANGAN1 <84792264+SHASHANKDEWANGAN1@users.noreply.github.com>
2021-05-26 15:40:11 +0530

    Greater of Three Numbers
    In README.md file is not  i have written the same code but Due to some issue it is not working and you will not get output. So this is the desired code Of Greater of Three Numbers.

---
+++ GreaterOfThreeNumbers.js
@@ -0,0 +1,9 @@
+var num1 = parseInt(prompt('Enter First Number')); 
+var num2 = parseInt(prompt('Enter Second Number'));
+var num3 = parseInt(prompt('Enter Third Number')); 
+  if (num1 > num2 && num1 > num3)
+  { document.write('Greatest Number is'+num1);} 
+  else if(num2 > num1 && num2 > num3)
+   { document.write('Greatest Number is'+num2);}
+   else
+    { document.write('Greatest Number is'+num3);}
