ad7a1fac8150fc941895695afc6aecbf548f07cf
SHASHANKDEWANGAN1 <84792264+SHASHANKDEWANGAN1@users.noreply.github.com>
2021-05-26 12:00:12 +0530

    JavaScriptFunctionAsAParameter
    Here i am going to introduce the JavaScript Function As A Parameter.

---
+++ JsFunctionAsParameter.js
@@ -0,0 +1,13 @@
+function hello( firstname = "Google", lastname ="dotcom") 
+{ 
+    document.write("Hello"  +  firstname  +"  "+  lastname  + "<br>");
+} 
+
+hello(); 
+function sum(x,y)
+{ 
+    document.write(x+y);
+
+}
+
+sum(71,37); 
