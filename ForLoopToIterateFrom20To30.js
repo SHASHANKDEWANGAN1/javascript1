7cbfffbbdde904dc97a9308bcd13b4202889c005
SHASHANKDEWANGAN1 <84792264+SHASHANKDEWANGAN1@users.noreply.github.com>
2021-05-26 12:42:01 +0530

    For Loop To Iterate From 20 To 30
    here, i am iterating the number from 20 to 30 and priting its status i.e. Odd Or Even

---
+++ ForLoopToIterateFrom20To30.js
@@ -0,0 +1,11 @@
+for (var a = 20; a<= 30; a++)
+{ 
+    if(a % 2 == 0)
+      { 
+          document.write(   a     +      "is even"   +    '<br>');
+      }
+       if(a % 2 !=0)
+         {
+          document.write(   a     +      "is odd"    +    "<br>");
+         } 
+}    
