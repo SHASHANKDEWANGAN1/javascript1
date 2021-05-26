15b2770d6882a3ad978070425a626e5fa3ccd953
SHASHANKDEWANGAN1 <84792264+SHASHANKDEWANGAN1@users.noreply.github.com>
2021-05-26 11:02:22 +0530

    Temperature Conversion
    here i am converting temperature form Faherenheit to Celcius and vice versa.

---
+++ TemperatureConversion.js
@@ -0,0 +1,7 @@
+var F=prompt('Enter Temperature In Faherenheit'); 
+var C =(F-32)*5/9; alert("Temperature in Celcius="+C); 
+document.write('Temperature In Celcius='+C); 
+var C = prompt('Enter Temperature In Celcius');
+var F=(C*1.8)+32; alert("Temperature in Faherenheit ="+F); 
+document.write('Temperature In Faherenheit ='+F);
+ 
