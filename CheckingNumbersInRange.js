435bbaf63dee31052258262607124ef589ae978c
SHASHANKDEWANGAN1 <84792264+SHASHANKDEWANGAN1@users.noreply.github.com>
2021-05-26 11:18:45 +0530

    CheckingNumber In Range of 50 to 100
    Here i am Checking the Number In the Range of 50 to 100. if not it will show Not in Range of 50 to 100.

---
+++ CheckingNumbersInRange.js
@@ -0,0 +1,10 @@
+var num1 = parseInt(prompt('Enter First Number'));
+
+if (num1>50 && num1 <100)
+ { 
+     document.write('This Number is in Range of 50 to 100','<br>');
+} 
+else 
+{ 
+    document.write('Not In Range Of 50 to 100','<br>');
+}
